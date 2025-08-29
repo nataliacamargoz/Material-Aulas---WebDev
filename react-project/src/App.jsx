797afccee1f0463aff287f9cd8e7414baea5340b
Natalia Camargo <nacamargo2909@gmail.com>
2025-08-28 22:40:40 -0300

    aula

---
+++ react-project/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+    return (
+    <>
+      <Header 
+      titulo="calleri"
+      texto="sp"
+      cor="red"/>
+      <Header 
+      titulo="lucas moura"
+      texto="sp"
+      cor="white"/>
+      <Header 
+      titulo="luciano"
+      cor="black"/>
+    </>
+  )
+}
+
+export default App
